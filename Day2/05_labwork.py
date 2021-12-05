61ffa805272cd5c5b40b55519afc74790a4fcdfe
Niraj Chaudhary <nirajc560@gmail.com>
2021-12-05 14:29:56 +0545

    day2

---
+++ Day2/05_labwork.py
@@ -0,0 +1,20 @@
+# labsession question 5
+class_a = int(input("Enter the number of student class_a"))
+class_b = int(input("Enter the number of student class_b"))
+class_c = int(input("Enter the number of student class_c"))
+A = class_a//2
+if(class_a%2 !=0):
+    A = (class_a//2)+1
+elif(class_a%2 ==0):
+    A = (class_a//2)
+B = class_b//2
+if(class_b%2 !=0):
+    B = (class_b//2)+1
+elif (class_b%2 ==0):
+    B = (class_b//2)
+C = class_c//2
+if(class_c%2 !=0):
+    C = (class_c//2)+2
+elif (class_c%2 ==0):
+    C = (class_c//2)
+print(f"The smallest number required is {A+B+C}")
---
+++ Day3/01_labwork.py
@@ -0,0 +1,1 @@
+# Check whether 5 is in list of first 5 natural numbers or not:
