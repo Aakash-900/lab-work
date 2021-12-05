3a1e3cb0f85a16bfa1246d23eb383c486c2ea093
Niraj Chaudhary <nirajc560@gmail.com>
2021-12-05 13:04:15 +0545

    update

---
+++ 06_labwork.py
@@ -0,0 +1,21 @@
+# labsessions question 6
+distance = 4
+bus_speed = 25/60    #converted mph to mpm(miles per minute)
+wait_time = 2
+stops = 10
+waiting = wait_time * stops
+bus_time = distance % bus_speed
+bus_totaltime = bus_time + waiting
+
+speed1 = 7*60       #converted mph to mpm(miles per minute)
+speed2 = 15*60
+time_7mpm = speed1*2
+time_15mpm = speed2*2
+jog_totaltime = time_7mpm + time_15mpm
+
+if bus_totaltime>jog_totaltime:
+    print(f"Bus will be Faster than Jogging.")
+elif bus_totaltime<jog_totaltime:
+    print(f"Jogging will be Faster than Travelling by Bus.")
+else:
+    print(f"Both Jogging and Travelling by Bus take same time.")
