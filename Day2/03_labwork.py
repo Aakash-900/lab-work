a4d0ea1da709cd1b1dd96e170998da4622ad79ca
Niraj Chaudhary <nirajc560@gmail.com>
2021-12-05 14:32:13 +0545

    a

--- Day2/03_labwork.py
+++
@@ -1,7 +0,0 @@
-# The number of minutes that is passed since midnight 
-No_Minutes = int(input("Enter the minute passed since midnight: "))
-Hours = (No_Minutes//60)
-Minutes =(No_Minutes % 60)
-print("f the hour is{hour}")
-print("f the minutes is{minute}")
-print("f{hour}:{minutes}")
--- Day2/04_labwork.py
+++
@@ -1,7 +0,0 @@
-# labsession question number 4
-No_students = int(input("Enter the number of students: "))
-No_apple = int(input("Enter the number of apple: "))
-Eachwillget = (No_apple//No_students)
-Remaining = (No_apple % No_students)
-print(f"Each student will get {Eachwillget}")
-print(f"No of apple will remain {Remaining}")
