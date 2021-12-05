6f307cf3ebb0371a7af00bd05d44a7ed19a07e5d
Aakash-900 <94778097+Aakash-900@users.noreply.github.com>
2021-12-05 14:03:14 +0545

    Update 06_labwork.py

--- 06_labwork.py
+++ 06_labwork.py
@@ -4,11 +4,11 @@ bus_speed = 25/60    #converted mph to mpm(miles per minute)
 wait_time = 2
 stops = 10
 waiting = wait_time * stops
-bus_time = distance % bus_speed
+bus_time = distance/bus_speed
 bus_totaltime = bus_time + waiting
 
-speed1 = 7*60       #converted mph to mpm(miles per minute)
-speed2 = 15*60
+speed1 = 7/60       #converted mph to mpm(miles per minute)
+speed2 = 15/60
 time_7mpm = speed1*2
 time_15mpm = speed2*2
 jog_totaltime = time_7mpm + time_15mpm
