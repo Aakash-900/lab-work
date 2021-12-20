5c412d357590dbdee803b00a8cdc30f053b7fca0
Aakash <aakash@Aakashs-MacBook-Pro.local>
2021-12-20 12:28:10 +0545

    update

--- Day 5/06_labwork.py
+++ Day 5/06_labwork.py
@@ -1,2 +1,17 @@
-#
-num = (1,2,3,4,5,6,7,8,9,10)
+num = [1,2,3,4,5,6,7,8,9] 
+
+even, odd= 0, 0
+
+for i in num: 
+
+    if i % 2 == 0: 
+
+        even += 1
+
+    else: 
+
+        odd+= 1          
+
+print("Even : ", even) 
+
+print("Odd : ", odd)
